c3b4eaf9479735f0e330993b54cc10c83178a049
Sam <sammiedegroot@gmail.com>
2024-04-09 13:08:00 +0200

    Allow collection to also be an array

--- functions/calculateSumOrAverage/1.0/index.js
+++ functions/calculateSumOrAverage/1.0/index.js
@@ -1,10 +1,10 @@
 const calculateSumOrAverage = async ({ collection, property, sumOrAvg }) => {
-  if (collection.data.length == 0) {
+  if (collection.data.length === 0) {
     return { as: 0 };
   }
   if (!collection.data[0].hasOwnProperty(property)) {
     throw new Error(
-      `The property name (${property}) you are trying to calculate the sum or average of does not exist in the collection.`
+      `The property name (${property}) you are trying to calculate the sum or average of does not exist in the collection.`,
     );
   }
   const floats = collection.data.map((c) =>
@@ -13,10 +13,11 @@ const calculateSumOrAverage = async ({ collection, property, sumOrAvg }) => {
 
   const totalSum = floats.reduce((a, c) => a + c, 0);
 
-  if (sumOrAvg == "sum") {
+  if (sumOrAvg === 'sum') {
     return { as: totalSum };
-  } else if (sumOrAvg == "average") {
-    return { as: totalSum / collection.data.length };
   }
+
+  return { as: totalSum / collection.data.length };
 };
+
 export default calculateSumOrAverage;

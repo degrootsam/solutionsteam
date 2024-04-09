fcb7d5074550ac59cbf01385507ec970421c250f
Sam <sammiedegroot@gmail.com>
2024-04-09 13:13:00 +0200

    Fix property check in calculateSumOrAverage function

--- functions/calculateSumOrAverage/1.0/index.js
+++ functions/calculateSumOrAverage/1.0/index.js
@@ -2,13 +2,13 @@ const calculateSumOrAverage = async ({ collection, property, sumOrAvg }) => {
   if (collection.data.length === 0) {
     return { as: 0 };
   }
-  if (!collection.data[0].hasOwnProperty(property)) {
+  if (!Object.prototype.hasOwnProperty.call(collection.data[0], property)) {
     throw new Error(
       `The property name (${property}) you are trying to calculate the sum or average of does not exist in the collection.`,
     );
   }
   const floats = collection.data.map((c) =>
-    c[property] === null ? 0 : parseFloat(c[property])
+    c[property] === null ? 0 : parseFloat(c[property]),
   );
 
   const totalSum = floats.reduce((a, c) => a + c, 0);
